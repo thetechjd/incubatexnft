cbcc2ef6a7ac38cdb1ec96db887eaafdbe19fe0f
Kevan Williams <kevwilliams012@gmail.com>
2022-08-10 16:28:03 -0500

    advisors added

--- src/components/Advisors/Advisors.js
+++ src/components/Advisors/Advisors.js
@@ -3,8 +3,48 @@ import '../Styles/style-front.css';
 import waqas from '../../assets/images/waqas.jpg';
 import maliha from '../../assets/images/maliha.png';
 import neel from '../../assets/images/neel.jpg';
-import sara from '../../assets/images/sara.jpg'
-import Namit from '../../assets/images/namit.jpg'
+import sara from '../../assets/images/sara.jpg';
+import Namit from '../../assets/images/namit.jpg';
+import AaronN from '../../assets/images/Aaron_Nightingale.jpg';
+import AndrewC from '../../assets/images/Andrew_Cripps.png';
+import AnkitaP from '../../assets/images/Ankita_Patel.jpg';
+import AntonioB from '../../assets/images/Antonio_Brasse.png';
+import BrentL from '../../assets/images/Brent_Lipschultz.jpg';
+import CamiR from '../../assets/images/Cami_Ragano.jpg';
+import CarynM from '../../assets/images/Caryn_Mefford.jpg';
+import ClaireL from '../../assets/images/Claire_Le_Compagnon.jpg';
+import DamienK from '../../assets/images/Damien_Kirchner.jpg';
+import DanaY from '../../assets/images/Dana_York.jpg';
+import DavidJ from '../../assets/images/David_Jonathan_Rogers.jpg';
+import Nayak from '../../assets/images/Debajyoti_Nayak.jpg';
+import DeborahG from '../../assets/images/Deborah_Gann.png';
+import DomenicT from '../../assets/images/Domenic_Thomas.jpg';
+import DuncanL from '../../assets/images/Duncan_Littlefield.jpg';
+import EboniB from '../../assets/images/Eboni_Blake.jpg';
+import FrederickG from '../../assets/images/Frederick_Grede.jpg';
+import GuadalupeC from '../../assets/images/Guadalupe_Cano.jpg';
+import IvanM from '../../assets/images/Ivan_Mlacak.jpg';
+import JadJ from '../../assets/images/Jad_Jubaili.jpg';
+import JeanetteS from '../../assets/images/Jeanette_Schreiber.jpg';
+import JoanneE from '../../assets/images/Joanne_Enders.jpg';
+import JoyceH from '../../assets/images/Joyce_Hunter.jpg';
+import JustinR from '../../assets/images/Justin_Rascode.jpg';
+import KateW from '../../assets/images/Kate_Williams.jpg';
+import KevinK from '../../assets/images/Kevin_Kelly.png';
+import LeeK from '../../assets/images/Lee_Keshav.png';
+import MiaG from '../../assets/images/Mia_Grable.jpg';
+import MickB from '../../assets/images/Mick_Badran.jpg';
+import MikeB from '../../assets/images/Mike_Batiste.jpg';
+import NicholasP from '../../assets/images/Nicholas_Payne.png';
+import PedroT from '../../assets/images/Pedro_Tavares.jpg';
+import RashaH from '../../assets/images/Rasha_Hamzeh.jpg';
+import RhondaN from '../../assets/images/Rhonda_Nolen.jpg';
+import RobertR from '../../assets/images/Robert_Radulescu.jpg';
+import ShilpaK from '../../assets/images/Shilpa_Karkeraa.jpg';
+import SimonJ from '../../assets/images/Simon_Julian.jpg';
+import SimoneS from '../../assets/images/Simone_Spencer.jpg';
+import TiffanyC from '../../assets/images/Tiffany_Calloway.png';
+import TylerH from '../../assets/images/Tyler_Harttraft.jpg';
 
 
 export default function Advisors() {
@@ -25,12 +65,214 @@ export default function Advisors() {
 						<p>Crypto Wizard</p>
 						<a href="https://www.linkedin.com/in/neel-desai-5670397a" target="_blank"><i class="fa fa-linkedin-square"></i></a>
 					</div>
-					<div class="team_info team-info-small" data-aos="fade-up">
+					<div class="team_info" data-aos="fade-up">
 						<img src={Namit} />
 						<h4>Namco</h4>
 						<p></p>
 						<a href="https://twitter.com/0xNamco" target="_blank"><i class="fab fa-twitter"></i></a>
 					</div>
+				</div>
+			</section>
+
+			<section class="team-section" data-aos="fade-up">
+
+				<div class="container">
+					<div class="team_info team-info-small" data-aos="fade-up">
+						<img src={AaronN} />
+						<h5 class="special-small">Aaron Nightingale</h5 >
+						<p></p>
+					</div>
+					<div class="team_info team-info-small" data-aos="fade-up">
+						<img src={AndrewC} />
+						<h5 class="special-small">Andrew Cripps</h5 >
+						<p></p>
+					</div>
+					<div class="team_info team-info-small" data-aos="fade-up">
+						<img src={AnkitaP} />
+						<h5 class="special-small">Ankita Patel</h5 >
+						<p></p>
+					</div>
+					<div class="team_info team-info-small" data-aos="fade-up">
+						<img src={AntonioB} />
+						<h5 class="special-small">Antonio Brasse</h5 >
+						<p></p>
+					</div>
+					<div class="team_info team-info-small" data-aos="fade-up">
+						<img src={BrentL} />
+						<h5 class="special-small">Brent Lipschultz</h5 >
+						<p></p>
+					</div>
+					<div class="team_info team-info-small" data-aos="fade-up">
+						<img src={CamiR} />
+						<h5 class="special-small">Cami Ragano</h5 >
+						<p></p>
+					</div>
+					<div class="team_info team-info-small" data-aos="fade-up">
+						<img src={CarynM} />
+						<h5 class="special-small">Caryn Mefford</h5 >
+						<p></p>
+					</div>
+					<div class="team_info team-info-small" data-aos="fade-up">
+						<img src={ClaireL} />
+						<h5 class="special-small">Claire Le Compagnon</h5 >
+						<p></p>
+					</div>
+					<div class="team_info team-info-small" data-aos="fade-up">
+						<img src={DamienK} />
+						<h5 class="special-small">Damien Kirchner</h5 >
+						<p></p>
+					</div>
+					<div class="team_info team-info-small" data-aos="fade-up">
+						<img src={DanaY} />
+						<h5 class="special-small">Dana York</h5 >
+						<p></p>
+					</div>
+					<div class="team_info team-info-small" data-aos="fade-up">
+						<img src={DavidJ} />
+						<h5 class="special-small">David Johnathan Rogers</h5 >
+						<p></p>
+					</div>
+					<div class="team_info team-info-small" data-aos="fade-up">
+						<img src={Nayak} />
+						<h5 class="special-small">Debajyoti Nayak</h5 >
+						<p></p>
+					</div>
+					<div class="team_info team-info-small" data-aos="fade-up">
+						<img src={DeborahG} />
+						<h5 class="special-small">Deborah Gann</h5 >
+						<p></p>
+					</div>
+					<div class="team_info team-info-small" data-aos="fade-up">
+						<img src={DomenicT} />
+						<h5 class="special-small">Domenic Thomas</h5 >
+						<p></p>
+					</div>
+					<div class="team_info team-info-small" data-aos="fade-up">
+						<img src={DuncanL} />
+						<h5 class="special-small">Duncan Littlefield</h5 >
+						<p></p>
+					</div>
+					<div class="team_info team-info-small" data-aos="fade-up">
+						<img src={EboniB} />
+						<h5 class="special-small">Eboni Blake</h5 >
+						<p></p>
+					</div>
+					<div class="team_info team-info-small" data-aos="fade-up">
+						<img src={FrederickG} />
+						<h5 class="special-small">Frederick Grede</h5 >
+						<p></p>
+					</div>
+					<div class="team_info team-info-small" data-aos="fade-up">
+						<img src={GuadalupeC} />
+						<h5 class="special-small">Guadalupe Cano</h5 >
+						<p></p>
+					</div>
+					<div class="team_info team-info-small" data-aos="fade-up">
+						<img src={IvanM} />
+						<h5 class="special-small">Ivan Mlacak</h5 >
+						<p></p>
+					</div>
+					<div class="team_info team-info-small" data-aos="fade-up">
+						<img src={JadJ} />
+						<h5 class="special-small">Jad Jubaili</h5 >
+						<p></p>
+					</div>
+					<div class="team_info team-info-small" data-aos="fade-up">
+						<img src={JeanetteS} />
+						<h5 class="special-small">Jeanette Schreiber</h5 >
+						<p></p>
+					</div>
+					<div class="team_info team-info-small" data-aos="fade-up">
+						<img src={JoanneE} />
+						<h5 class="special-small">Joanne Enders</h5 >
+						<p></p>
+					</div>
+					<div class="team_info team-info-small" data-aos="fade-up">
+						<img src={JoyceH} />
+						<h5 class="special-small">Joyce Hunter</h5 >
+						<p></p>
+					</div>
+					<div class="team_info team-info-small" data-aos="fade-up">
+						<img src={JustinR} />
+						<h5 class="special-small">Justin Rascode</h5 >
+						<p></p>
+					</div>
+					<div class="team_info team-info-small" data-aos="fade-up">
+						<img src={KateW} />
+						<h5 class="special-small">Kate Williams, Ph.D</h5 >
+						<p></p>
+					</div>
+					<div class="team_info team-info-small" data-aos="fade-up">
+						<img src={KevinK} />
+						<h5 class="special-small">Kevin Kelly</h5 >
+						<p></p>
+					</div>
+					<div class="team_info team-info-small" data-aos="fade-up">
+						<img src={LeeK} />
+						<h5 class="special-small">Lee Keshav</h5 >
+						<p></p>
+					</div>
+					<div class="team_info team-info-small" data-aos="fade-up">
+						<img src={MiaG} />
+						<h5 class="special-small">Mia Grable</h5 >
+						<p></p>
+					</div>
+					<div class="team_info team-info-small" data-aos="fade-up">
+						<img src={MickB} />
+						<h5 class="special-small">Mick Badran</h5 >
+						<p></p>
+					</div>
+					<div class="team_info team-info-small" data-aos="fade-up">
+						<img src={MikeB} />
+						<h5 class="special-small">Mike Batiste</h5 >
+						<p></p>
+					</div>
+					<div class="team_info team-info-small" data-aos="fade-up">
+						<img src={NicholasP} />
+						<h5 class="special-small">Nicholas Payne</h5 >
+						<p></p>
+					</div>
+					<div class="team_info team-info-small" data-aos="fade-up">
+						<img src={PedroT} />
+						<h5 class="special-small">Pedro Tavares</h5 >
+						<p></p>
+					</div>
+					<div class="team_info team-info-small" data-aos="fade-up">
+						<img src={RashaH} />
+						<h5 class="special-small">Rasha Hamzeh</h5 >
+						<p></p>
+					</div>
+					<div class="team_info team-info-small" data-aos="fade-up">
+						<img src={RhondaN} />
+						<h5 class="special-small">Rhonda Nolen</h5 >
+						<p></p>
+					</div>
+					<div class="team_info team-info-small" data-aos="fade-up">
+						<img src={ShilpaK} />
+						<h5 class="special-small">Shilpa Karkeraa</h5 >
+						<p></p>
+					</div>
+					<div class="team_info team-info-small" data-aos="fade-up">
+						<img src={SimonJ} />
+						<h5 class="special-small">Simon Julian</h5 >
+						<p></p>
+					</div>
+					<div class="team_info team-info-small" data-aos="fade-up">
+						<img src={SimoneS} />
+						<h5 class="special-small">Simone Spencer</h5 >
+						<p></p>
+					</div>
+					<div class="team_info team-info-small" data-aos="fade-up">
+						<img src={TiffanyC} />
+						<h5 class="special-small">Tiffany Calloway</h5 >
+						<p></p>
+					</div>
+					<div class="team_info team-info-small" data-aos="fade-up">
+						<img src={TylerH} />
+						<h5 class="special-small">Tyler Harttraft</h5 >
+						<p></p>
+					</div>
+
 
 				</div>
 			</section>
--- src/components/CoreTeam/CoreTeam.js
+++ src/components/CoreTeam/CoreTeam.js
@@ -1,56 +1,56 @@
 import React from 'react'
 import '../Styles/style-front.css';
-import Fatima  from '../../assets/images/fatima.png'
-import Massy  from '../../assets/images/massy.jpg'
-import Ali  from '../../assets/images/ali.png'
-import Bilal  from '../../assets/images/bilal.jpg'
-import George  from '../../assets/images/george.jpg'
-import Namit  from '../../assets/images/namit.jpg'
+import Fatima from '../../assets/images/fatima.png'
+import Massy from '../../assets/images/massy.jpg'
+import Ali from '../../assets/images/ali.png'
+import Bilal from '../../assets/images/bilal.jpg'
+import George from '../../assets/images/george.jpg'
+import Namit from '../../assets/images/namit.jpg'
 import waqas from '../../assets/images/waqas.jpg';
 
 
 export default function CoreTeam() {
-    return (
-        <div>
-      {/* <!--team section--> */}
-<section class="team-section" id="team" data-aos="fade-up">
-	<div class="container">
-			   <h3>TRIBE LEADERS</h3>
-				<div class="team_info team-info-small" data-aos="fade-right">
-				  <img src={Massy}/>
-				  <h4>Massy</h4>
-				  <p>The Sage</p>
-				  <a href="https://www.linkedin.com/in/massy-almubidin-2b18031aa/" target="_blank"><i class="fa fa-linkedin-square"></i></a>
-				  <a href="https://twitter.com/massymace" target="_blank"><i class="fab fa-twitter"></i></a>
-				</div>
+	return (
+		<div>
+			{/* <!--team section--> */}
+			<section class="team-section" id="team" data-aos="fade-up">
+				<div class="container">
+					<h3>TRIBE LEADERS</h3>
+					<div class="leaders" data-aos="fade-right">
+						<img src={Massy} />
+						<h4>Massy</h4>
+						<p>The Sage</p>
+						<a href="https://www.linkedin.com/in/massy-almubidin-2b18031aa/" target="_blank"><i class="fa fa-linkedin-square"></i></a>
+						<a href="https://twitter.com/massymace" target="_blank"><i class="fab fa-twitter"></i></a>
+					</div>
 
-				<div class="team_info team-info-small" data-aos="fade-right">
-				  <img src={waqas}/>
-				  <h4>Waqas</h4>
-				  <p>The Magician</p>
-				  <a href="https://www.linkedin.com/in/waqas-hassan-180732104/" target="_blank"><i class="fa fa-linkedin-square"></i></a>
-				  {/* <a href="https://twitter.com/WaqasHasX" target="_blank"><i class="fab fa-twitter"></i></a> */}
-				</div>
+					<div class="leaders" data-aos="fade-right">
+						<img src={waqas} />
+						<h4>Waqas</h4>
+						<p>The Magician</p>
+						<a href="https://www.linkedin.com/in/waqas-hassan-180732104/" target="_blank"><i class="fa fa-linkedin-square"></i></a>
+						{/* <a href="https://twitter.com/WaqasHasX" target="_blank"><i class="fab fa-twitter"></i></a> */}
+					</div>
 
-				<div class="team_info team-info-small" data-aos="fade-up">
-				  <img src={Bilal}/>
-				  <h4>Bilal</h4>
-				  <p>The Everyman</p>
-				  <a href="https://www.linkedin.com/in/mohammad-pirzada-49a22291" target="_blank"><i class="fa fa-linkedin-square"></i></a>
-				  <a href="https://twitter.com/bilalpirzada3" target="_blank"><i class="fab fa-twitter"></i></a>
-				</div>
+					<div class="leaders " data-aos="fade-up">
+						<img src={Bilal} />
+						<h4>Bilal</h4>
+						<p>The Everyman</p>
+						<a href="https://www.linkedin.com/in/mohammad-pirzada-49a22291" target="_blank"><i class="fa fa-linkedin-square"></i></a>
+						<a href="https://twitter.com/bilalpirzada3" target="_blank"><i class="fab fa-twitter"></i></a>
+					</div>
 
-				<div class="team_info team-info-small" data-aos="fade-left">
-				  <img src={George}/>
-				  <h4>George</h4>
-				  <p>The Artist</p>
-				  <a href="https://www.linkedin.com/in/george-mikhael-7573b0143/" target="_blank"><i class="fa fa-linkedin-square"></i></a>
-				  <a href="https://twitter.com/Georgemikhaell" target="_blank"><i class="fab fa-twitter"></i></a>
-				</div>
+					<div class="leaders " data-aos="fade-left">
+						<img src={George} />
+						<h4>George</h4>
+						<p>The Artist</p>
+						<a href="https://www.linkedin.com/in/george-mikhael-7573b0143/" target="_blank"><i class="fa fa-linkedin-square"></i></a>
+						<a href="https://twitter.com/Georgemikhaell" target="_blank"><i class="fab fa-twitter"></i></a>
+					</div>
 
-				<button onClick={()=>{window.location.href="/allteam"}} class="submit-project-btn" type="submit" ><span>More about the team</span></button>
+					<button onClick={() => { window.location.href = "/allteam" }} class="submit-project-btn" type="submit" ><span>More about the team</span></button>
+				</div>
+			</section>
 		</div>
-</section>
-        </div>
-    )
+	)
 }
