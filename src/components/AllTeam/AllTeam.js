5622e289db3d464c50a29457942adc6a194835b7
Kevan Williams <kevwilliams012@gmail.com>
2022-08-13 14:50:02 -0500

    kevan bio

--- src/components/AllTeam/AllTeam.js
+++ src/components/AllTeam/AllTeam.js
@@ -157,7 +157,8 @@ export default function AllTeam() {
                             <h4>Kevan</h4>
                             <span></span>
                             <a href="https://www.linkedin.com/in/kevan-williams-317a80a1/" target="_blank"><i class="fa fa-linkedin-square"></i></a>
-                            <p></p>
+                            <p>Kevan Williams, Esq. is the Founder and CEO of Ascendant.Finance, with the aim of helping small businesses to take advantage of the alternative financing options made available by safely transitioning from web2 to web3. His portfolio to date includes over 100 projects. </p>
+                            <p>His career in web3 began as early as 2017, while he was still attending Michigan State University College of Law, when he became fascinated with smart contracts and the opportunities they presented to allow open-source developers greater control over licensing and monetization of their software. Being a developer himself, Kevan enjoys being intimately involved with all aspects of the projects he takes on. His passion for the technology drives him to continue learning and pushing the boundaries of the space.</p>
                         </div>
                     </div>
                     <div class="full-team-info" data-aos="fade-left">
