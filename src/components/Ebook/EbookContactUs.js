9913abdea89202320845ad526385d85c508acaec
Kevan Williams <kevwilliams012@gmail.com>
2022-08-24 18:16:57 -0500

    size changes

--- src/components/Ebook/EbookContactUs.js
+++ src/components/Ebook/EbookContactUs.js
@@ -43,7 +43,7 @@ export default function EbookDownload() {
                 <section class="contact-section" id="contact" data-aos="fade-up" style={{ backgroundColor: '#693ecb' }}>
                     <div class="container" style={{ backgroundColor: '#693ecb' }}>
                         <div id="top" style={{ justifyContent: 'center' }}>
-                            <div style={{ width: '33%', position: 'absolute', marginTop: '10%', marginLeft: '10%' }}>
+                            <div style={{ width: '33%', position: 'absolute', marginTop: '10%', marginLeft: '5%' }}>
                                 <p style={{ fontSize: '3vw', lineHeight: '150%' }}>PLAN & EXECUTE YOUR NFT PROJECT LIKE A PRO!</p>
                                 <p style={{ fontSize: '1.5vw', lineHeight: '100%' }}>If you have a good idea for an NFT project but feel lost in the space and unsure how to proceed, this free Ebook guide is for you! We will walk you step by step through the most important stages of planning & executing your project with easy to follow tips & resources!</p>
                             </div>
@@ -51,20 +51,20 @@ export default function EbookDownload() {
                             <img src={top} style={{ width: '100%', backgroundColor: '#edfcfd', marginBottom: '-5px' }} />
                         </div>
                         <div id="build" style={{ justifyContent: 'center' }}>
-                            <div style={{ width: '33%', position: 'absolute', marginTop: '2%', marginLeft: '55%' }}>
-                                <p style={{ color: '#2e2e2d', fontSize: '3vw', lineHeight: '150%' }}>LEARN HOW TO BUILD A SUCCESSFUL NFT PROJECT IN TODAY'S MARKET!</p>
-                                <p style={{ color: '#2e2e2d', fontSize: '1.5vw', lineHeight: '150%' }}>The Structure That Successful Projects Follow</p>
-                                <p style={{ color: '#2e2e2d', fontSize: '1.5vw', lineHeight: '100%' }}>It can be scary to attempt to build in a brand new market in today's conditions. This guide will equip you with resources to maximize your chances of success even in a bear(ish) market!</p>
+                            <div style={{ width: '40%', position: 'absolute', marginTop: '2%', marginLeft: '48%' }}>
+                                <p style={{ paddingRight: '30%', color: '#2e2e2d', fontSize: '3vw', lineHeight: '150%' }}>LEARN HOW TO BUILD A SUCCESSFUL NFT PROJECT IN TODAY'S MARKET!</p>
+                                <p style={{ paddingRight: '30%', color: '#2e2e2d', fontSize: '1.5vw', lineHeight: '150%' }}>The Structure That Successful Projects Follow</p>
+                                <p style={{ paddingRight: '30%', color: '#2e2e2d', fontSize: '1.5vw', lineHeight: '100%' }}>It can be scary to attempt to build in a brand new market in today's conditions. This guide will equip you with resources to maximize your chances of success even in a bear(ish) market!</p>
                             </div>
                             <img src={build} style={{ width: '100%', marginTop: '-5px', marginRight: '2em', backgroundColor: '#edfcfd' }} />
 
 
                         </div>
-                        <div id="mistakes" style={{ justifyContent: 'center' }}>
-                            <div style={{ textAlign: 'center', width: '90%', position: 'absolute', marginTop: '5%' }}>
-                                <p style={{ paddingLeft: '10%', paddingRight: '10%', fontSize: '3.5vw', lineHeight: '150%' }}>AVOID COSTLY MISTAKES & LEARN FROM THE EXPERTS! </p>
-                                <p style={{ paddingLeft: '10%', paddingRight: '10%', fontSize: '2vw', lineHeight: '150%' }}>You Don't Need To Reinvent The Wheel! </p>
-                                <p style={{ paddingLeft: '10%', paddingRight: '10%', fontSize: '2vw', lineHeight: '150%' }}>This free Ebook guide covers the main areas that most web3 creators need to address before launching if they plan to have a sold out mint. There's more to a successful launch than just putting out an art collection on the blockchain and this Ebook will guide you through what you need to do!</p>
+                        <div id="mistakes" >
+                            <div style={{ textAlign: 'center', width: '95%', position: 'absolute', marginTop: '5%' }}>
+                                <p style={{ paddingLeft: '10%', paddingRight: '20%', fontSize: '3vw', lineHeight: '150%' }}>AVOID COSTLY MISTAKES & LEARN FROM THE EXPERTS! </p>
+                                <p style={{ paddingLeft: '10%', paddingRight: '20%', fontSize: '2vw', lineHeight: '150%' }}>You Don't Need To Reinvent The Wheel! </p>
+                                <p style={{ paddingLeft: '10%', paddingRight: '20%', fontSize: '2vw', lineHeight: '150%' }}>This free Ebook guide covers the main areas that most web3 creators need to address before launching if they plan to have a sold out mint. There's more to a successful launch than just putting out an art collection on the blockchain and this Ebook will guide you through what you need to do!</p>
 
                             </div>
                             <img src={mistakes} style={{ width: '100%', marginTop: '-5px', marginRight: '2em', backgroundColor: '#edfcfd' }} />
@@ -92,11 +92,11 @@ export default function EbookDownload() {
 
 
 
-                            <div id="my-form" class="form-content" style={{ backgroundColor: '#693ecb', marginTop: '1em', padding: '0 5%', justifyContent: 'center' }}>
+                            <div id="my-form" class="form-content" style={{ maxWidth: '95%', backgroundColor: '#693ecb', marginTop: '1em', padding: '0 5%', justifyContent: 'center' }}>
                                 <form action="https://docs.google.com/forms/d/e/1FAIpQLSfb1rVWkz-bOF5-L7TskktbvJavMv9VtJctsGf8YHmhVLZ3ag/formResponse" method="post" onSubmit={handleSubmit} target="_blank">
                                     <h2>Download Your Free Ebook</h2>
-                                    <input type="text" id="fname" style={{ width: '95%', color: '#000', backgroundColor: '#fff' }} name="entry.1911725452" placeholder="Name*" />
-                                    <input type="email" id="mail" style={{ width: '95%', color: '#000', backgroundColor: '#fff' }} name="entry.917704839" placeholder="Email*" required /><br /><br />
+                                    <input type="text" id="fname" style={{ color: '#000', backgroundColor: '#fff' }} name="entry.1911725452" placeholder="Name*" />
+                                    <input type="email" id="mail" style={{ color: '#000', backgroundColor: '#fff' }} name="entry.917704839" placeholder="Email*" required /><br /><br />
                                     <button type="submit" id="download-pdf" class='download-btn'><a href={whitepaper} download><span>Download</span></a></button>
                                 </form>
 
